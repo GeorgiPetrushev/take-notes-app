66f2c6f614003c0989e24ce1bc5cc6663abd358a
Georgi <figoto660@gmail.com>
2022-09-08 22:04:39 -0700

    create note and note list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import NoteList from "./components/NoteList";
 
-const App = () => {
+const notes = [
+  {
+  id:"1",
+  text: "note one",
+  data: "10/10/2022"
+},
+{
+  id:"1",
+  text: "note 2",
+  data: "10/10/2022"
+},
+{
+  id:"1",
+  text: "note 3",
+  data: "10/10/2022"
+}
+]
+
+const App = (prop) => {
   return (
     <div>
-      <NoteList />
+      <NoteList prop={notes}/>
     </div>
   );
 };
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Note from "./Note";
 
-const NoteLins = () => {
-  return (
-    <div>
-      <Note />
-    </div>
-  );
+const NoteLins = (notes) => {
+  console.log(notes);
+  console.log(notes.prop);
+  const newNoteList = notes.prop.map(() => <Note />);
+  return <div>{newNoteList}</div>;
 };
 
 export default NoteLins;
